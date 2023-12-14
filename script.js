8c0bc5295d43512ad6f0fbed1487deaaea069ad7
Blazej-oWo <b.wojtusciszyn@gmail.com>
2023-12-14 17:18:00 +0100

    mobile almost done

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-const hamMenu = document.querySelector('ham-menu');
+const hamburger = document.querySelector('.hamburger');
 
-const offScreenMenu = document.querySelector('.off-screen-menu');
+const navMenu = document.querySelector('.nav-menu');
 
-hamMenu.addEventListener('click', () => {
-    hamMenu.classList.toggle('active');
-    offScreenMenu.classList.toggle('active');
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
 })
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
